273c7e0dd4ee66af0ede8aa2deb7be01c1a19455
cuzperf <cuzperf@outlook.com>
2022-12-08 13:17:01 +0800

    update

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ git checkout --orphan tmp
 git clone git@github.com:cuzperf/agoralite.git
 git clone git@github.com:cuzperf/blog.git
 git clone git@github.com:cuzperf/cf.git
+git clone git@github.com:cuzperf/cplib.git
+git clone git@github.com:cuzperf/resume.git
+git clone git@github.com:cuzperf/standalone_gn.git
 git clone git@github.com:cuzperf/ics2021pa.git
 git clone git@github.com:cuzperf/mix.git
 
