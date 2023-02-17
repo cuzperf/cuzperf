e94e41c6ee4af5e53349199e449504e73ae9c056
cuzperf <cuzperf@outlook.com>
2023-02-18 00:40:24 +0800

    refine

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-git checkout --orphan tmp
+git config --global user.name cuzperf
+git config --global user.email cuzperf@outlook.com
+git config --global core.longpaths true
 
 git clone git@github.com:cuzperf/agoralite.git
 git clone git@github.com:cuzperf/blog.git
@@ -14,12 +16,3 @@ git clone git@github.com:cuzperf/resume.git
 git clone git@github.com:cuzperf/standalone_gn.git
 git clone git@github.com:cuzperf/ubuntu.git
 
-git remote add origin git@github.com:cuzperf/cuzperf.git
-git config --local user.name cuzperf
-git config --local user.email cuzperf@outlook.com
-git add -A
-git commit -am "init"
-git branch -D master
-git branch -M master
-
-# git push -f --set-upstream origin master
